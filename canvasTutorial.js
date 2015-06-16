9a77e6916ac384114644ac599519d016a0ca7941
amisnik <andreymysnyk@gmail.com>
2015-06-16 16:24:17 +0300

    growing tree

--- canvasTutorial.js
+++ canvasTutorial.js
@@ -1,83 +1,118 @@
 // Created by amisnik on 11.06.2015.
 
-function draw() {
+var rootCoef = 0.03;
+var subBranchMinLength = 10;
+var subBranchLength = 300;
+var subBranchNeighbour = 0.7;
+
+var drawInProgress = false;
+
+function draw(age) {
 	var canvas = document.getElementById('tutorial');
 
-	if (canvas.getContext) {
+	if (canvas.getContext && !drawInProgress) {
 		var ctx = canvas.getContext('2d');
 
-		roundedRect(ctx,12,12,150,150,15);
-		roundedRect(ctx,19,19,150,150,9);
-		roundedRect(ctx,53,53,49,33,10);
-		roundedRect(ctx,53,119,49,16,6);
-		roundedRect(ctx,135,53,49,33,10);
-		roundedRect(ctx,135,119,25,49,10);
-
+		ctx.fillStyle = 'white';
 		ctx.beginPath();
-		ctx.arc(37,37,13,Math.PI/7,-Math.PI/7,false);
-		ctx.lineTo(31,37);
+		ctx.rect(0, 0, 600, 600);
 		ctx.fill();
 
-		for(var i=0;i<8;i++){
-			ctx.fillRect(51+i*16,35,4,4);
-		}
+		drawInProgress = true;
 
-		for(i=0;i<6;i++){
-			ctx.fillRect(115,51+i*16,4,4);
-		}
+		drawBranch(ctx, {x: -180 + age, y: 600}, {x: 780 - age, y: 600}, 0);
 
-		for(i=0;i<8;i++){
-			ctx.fillRect(51+i*16,99,4,4);
-		}
-
-		ctx.beginPath();
-		ctx.moveTo(83,116);
-		ctx.lineTo(83,102);
-		ctx.bezierCurveTo(83,94,89,88,97,88);
-		ctx.bezierCurveTo(105,88,111,94,111,102);
-		ctx.lineTo(111,116);
-		ctx.lineTo(106.333,111.333);
-		ctx.lineTo(101.666,116);
-		ctx.lineTo(97,111.333);
-		ctx.lineTo(92.333,116);
-		ctx.lineTo(87.666,111.333);
-		ctx.lineTo(83,116);
-		ctx.fill();
-
-		ctx.fillStyle = "white";
-		ctx.beginPath();
-		ctx.moveTo(91,96);
-		ctx.bezierCurveTo(88,96,87,99,87,101);
-		ctx.bezierCurveTo(87,103,88,106,91,106);
-		ctx.bezierCurveTo(94,106,95,103,95,101);
-		ctx.bezierCurveTo(95,99,94,96,91,96);
-		ctx.moveTo(103,96);
-		ctx.bezierCurveTo(100,96,99,99,99,101);
-		ctx.bezierCurveTo(99,103,100,106,103,106);
-		ctx.bezierCurveTo(106,106,107,103,107,101);
-		ctx.bezierCurveTo(107,99,106,96,103,96);
-		ctx.fill();
-
-		ctx.fillStyle = "black";
-		ctx.beginPath();
-		ctx.arc(101,102,2,0,Math.PI*2,true);
-		ctx.fill();
-
-		ctx.beginPath();
-		ctx.arc(89,102,2,0,Math.PI*2,true);
-		ctx.fill();	}
+		drawInProgress = false;
+	}
 }
 
-function roundedRect(ctx,x,y,width,height,radius){
+function drawBranch(ctx, start, end, level) {
+	if (level > 10) return;
+
+	var left = getPointAt(start, end, 0.5 - rootCoef);
+	var right = getPointAt(start, end, 0.5 + rootCoef);
+	var top = getPermPointAt(start, end, -0.6);
+
+	ctx.fillStyle = 'rgb(160, 85, 45)';
+
 	ctx.beginPath();
-	ctx.moveTo(x,y+radius);
-	ctx.lineTo(x,y+height-radius);
-	ctx.quadraticCurveTo(x,y+height,x+radius,y+height);
-	ctx.lineTo(x+width-radius,y+height);
-	ctx.quadraticCurveTo(x+width,y+height,x+width,y+height-radius);
-	ctx.lineTo(x+width,y+radius);
-	ctx.quadraticCurveTo(x+width,y,x+width-radius,y);
-	ctx.lineTo(x+radius,y);
-	ctx.quadraticCurveTo(x,y,x,y+radius);
-	ctx.stroke();
+	ctx.moveTo(left.x, left.y);
+	ctx.lineTo(top.x, top.y);
+	ctx.lineTo(right.x, right.y);
+	ctx.closePath();
+
+	ctx.fill();
+
+	// left branch
+	drawBranchInnerLeft(ctx, left, top, 0.1, level);
+
+	// right branch
+	drawBranchInnerRight(ctx, top, right, 0.05, level);
+}
+
+function drawBranchInnerLeft(ctx, start, end, initialOffset, level) {
+	var length = distance(start, end);
+
+	if (length > subBranchMinLength) {
+		var koef = length > subBranchLength ? length / subBranchLength : 1;
+		var subBranch = getPointAt(start, end, subBranchNeighbour + initialOffset);
+
+		drawBranch(ctx, start, subBranch, level + 1);
+
+		var middle = getPointAt(start, subBranch, 0.5);
+
+		drawBranchInnerLeft(ctx, middle, end, 0, level);
+	} else {
+		drawLeaf(ctx, start, end);
+	}
+}
+
+function drawBranchInnerRight(ctx, start, end, initialOffset, level) {
+	var length = distance(start, end);
+
+	if (length > subBranchMinLength) {
+		var koef = length > subBranchLength ? length / subBranchLength : 1;
+		var subBranch = getPointAt(start, end, (1 - subBranchNeighbour) - initialOffset);
+
+		drawBranch(ctx, subBranch, end, level + 1);
+
+		var middle = getPointAt(subBranch, end, 0.5);
+
+		drawBranchInnerRight(ctx, start, middle, 0, level);
+	} else {
+		drawLeaf(ctx, start, end);
+	}
+}
+
+function drawLeaf(ctx, start, end) {
+	var middle = getPointAt(start, end, 0.5);
+
+	ctx.fillStyle = 'rgba(0, 160, 0, 0.1)';
+
+	ctx.beginPath();
+	ctx.arc(middle.x, middle.y, subBranchMinLength, 0, 2 * Math.PI);
+
+	ctx.closePath();
+
+	ctx.fill();
+}
+
+function distance(start, end) {
+	return Math.max(Math.abs(start.x - end.x), Math.abs(start.y - end.y));
+}
+
+function getPointAt(start, end, t) {
+	return {
+		x: (end.x - start.x) * t + start.x,
+		y: (end.y - start.y) * t + start.y
+	};
+}
+
+function getPermPointAt(start, end, t) {
+	var middle = getPointAt(start, end, 0.5);
+
+	return {
+		x: (start.y - end.y) * t + middle.x,
+		y: (end.x - start.x) * t + middle.y
+	}
 }
